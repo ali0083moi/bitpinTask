e7a25bdf8697fbc9924a732c3fff0549b1381591
ali0083moi <ali0083moghadasi.mypc@gmail.com>
2024-11-26 00:55:39 +0330

    some small changes in settings to add cache

--- bitpinTask/settings.py
+++ bitpinTask/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "posts",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -125,10 +126,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
     }
 }
