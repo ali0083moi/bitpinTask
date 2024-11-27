e9e897b4867ba99172f78b14184b09f3c74b5627
ali0083moi <ali0083moghadasi.mypc@gmail.com>
2024-11-27 16:34:52 +0330

    add user count to the post serializer

--- posts/serializers/__post_serializer.py
+++ posts/serializers/__post_serializer.py
@@ -6,6 +6,7 @@ from .__rate_serializer import RateSerializer
 class PostSerializer(serializers.ModelSerializer):
     average_rate = serializers.SerializerMethodField()
     user_rate = serializers.SerializerMethodField()
+    user_rate_count = serializers.SerializerMethodField()
 
     def get_average_rate(self, obj):
         return getattr(obj, "_cached_average_score", obj.average_score)
@@ -17,6 +18,9 @@ class PostSerializer(serializers.ModelSerializer):
             return rate.score if rate else None
         return None
 
+    def get_user_rate_count(self, obj):
+        return obj.rates.filter(is_active=True).count() or 0
+
     class Meta:
         model = Post
         fields = [
@@ -26,6 +30,7 @@ class PostSerializer(serializers.ModelSerializer):
             "author",
             "average_rate",
             "user_rate",
+            "user_rate_count",
             "created_at",
             "updated_at",
         ]
