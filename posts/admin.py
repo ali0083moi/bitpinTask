8124f80bd345cd31459b94b145008daf19d34edf
ali0083moi <ali0083moghadasi.mypc@gmail.com>
2024-11-27 18:32:20 +0330

    add models to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Rate
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "author", "created_at"]
+    search_fields = ["title", "author__username"]
+    list_filter = ["created_at"]
+
+
+admin.site.register(Post, PostAdmin)
+
+
+class RateAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "post", "score", "created_at"]
+    search_fields = ["user__username", "post__title"]
+    list_filter = ["created_at"]
+
+
+admin.site.register(Rate, RateAdmin)
